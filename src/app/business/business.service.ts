7be19162570a5faf05aa1fad9b4af4ce2e46cab2
Geziel Carvalho <geziel.natal@gmail.com>
2024-09-01 12:42:27 -0400

    Replace mocked data by API

--- src/app/business/business.service.ts
+++ src/app/business/business.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IBusiness } from './business.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BusinessService {
-  private apiUrl = 'http://api-url.com/businesses';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://antlia-mockapi.azurewebsites.net/api/v1/itau_teste',
+};
